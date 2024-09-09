763cf7a5e1717962bde249fa840f5f9f8ade70d7
Arvind <arvind.kumar@accolitedigital.com>
2024-09-09 15:29:26 +0530

    added firebase notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { RootNavigator } from './navigation/RootNavigator';
-import { store } from './store/store'
-import { Provider } from 'react-redux'
+import React, {useEffect} from 'react';
+import {Alert, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {RootNavigator} from './navigation/RootNavigator';
+import {store} from './store/store';
+import {Provider} from 'react-redux';
+import messaging from '@react-native-firebase/messaging';
+import {requestNotificationPermission} from './utility/runtimePermission';
 
 function App(): React.JSX.Element {
+  const permissionSuccessCallback = () => {
+    console.log('Notification permission granted =====');
+    // Get the device token
+    const getToken = async () => {
+      const token = await messaging().getToken();
+      console.log('Device FCM Token:======', token);
+    };
+
+    getToken();
+  };
+  const permissionFailureCallback = () => {
+    Alert.alert('Notification permission not granted!!!');
+  };
+
+  useEffect(() => {
+    // requestNotificationPermission(
+    //   permissionSuccessCallback,
+    //   permissionFailureCallback,
+    // );
+    // Get the device token
+    const getToken = async () => {
+      const token = await messaging().getToken();
+      console.log('Device FCM Token:======', token);
+    };
+
+    getToken();
+    // Listen for incoming messages
+    messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+  }, []);
 
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <RootNavigator/>
+        <RootNavigator />
       </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- src/utility/runtimePermission.tsx
+++ src/utility/runtimePermission.tsx
@@ -1,26 +1,48 @@
-import { PermissionsAndroid } from "react-native";
+import {PermissionsAndroid} from 'react-native';
 
 export const requestWriteStoragePermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-        {
-          title: 'Need Device storage write permission to store data',
-          message:
-            'NA ' ,
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the storage');
-      } else {
-        console.log('storage permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        title: 'Need Device storage write permission to store data',
+        message: 'NA ',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the storage');
+    } else {
+      console.log('storage permission denied');
     }
-  };
-  
-  
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+export const requestNotificationPermission = async (
+  successCallback: any,
+  failureCallback: any,
+) => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      {
+        title: 'Request for Notification',
+        message: 'This app needs notification permission.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      successCallback();
+    } else {
+      failureCallback();
+    }
+  } catch (err) {
+    failureCallback(err);
+  }
+};
